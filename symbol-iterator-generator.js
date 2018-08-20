6c975c5e98a949b2fa57055f5cb88723704adfc6
Rumman Ahmed <ahmed.rumman@gmail.com>
2018-08-20 10:09:14 +1000

    iterator

---
+++ symbol-iterator-generator.js
@@ -0,0 +1,55 @@
+//Symbol
+
+var x = Symbol('whatever is name')
+
+var obj = {
+	id: 42
+	//[x]:'secret value I am putting here'
+
+}
+
+obj[x] ="this is symbol value "
+
+console.log(obj);
+console.log("getOwnPropertyNames ",Object.getOwnPropertyNames(obj))
+console.log("getOwnPropertySymbols ",Object.getOwnPropertySymbols(obj))
+
+
+//iterator
+
+var arr = [1,2,3], 
+	str = 'pencil';
+
+var [x,y,...rest] = [...str]
+
+console.log("first 2 charecter of str is ", x,y)
+console.log('And rest is ', rest)
+var it = arr[Symbol.iterator]();
+
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+
+//for loop
+	for (let v=0;v<arr.length;v++){
+		console.log("for", v)
+	}
+// for in loop 
+	for (let k in str){
+		console.log("for in ", k, " =>", str[k])
+	}
+//for of loop
+	for (let v of str){
+		console.log("for of ", v)
+	}
+
+
+
+
+
+
+
+
+
+
